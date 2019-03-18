50f85d2e9419eaced882a693c70a4f7430eda7e3
pyrin andrii <pyrin.andrii@gmail.com>
2019-03-18 22:50:59 +0200

    Add image column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_202308) do
+ActiveRecord::Schema.define(version: 2019_03_18_204257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_03_18_202308) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
